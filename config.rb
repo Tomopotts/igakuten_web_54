390e755abd9ed32f2169eff488f26eaa0b469683
Tomopotts <tomo.rrow.11235@gmail.com>
2015-03-13 17:33:18 +0900

    add first img

--- config.rb
+++ config.rb
@@ -37,7 +37,7 @@
 
 # Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload, :host => 'Tomo-PC'.chomp
+  activate :livereload, :host => 'pc-PC'.chomp
 end
 
 # Methods defined in the helpers block are available in templates
