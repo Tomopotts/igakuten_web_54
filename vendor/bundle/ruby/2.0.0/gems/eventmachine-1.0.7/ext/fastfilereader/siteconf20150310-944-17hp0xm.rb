3a974e75e422e6634616fe1bad2b03ca6045decd
Tomopotts <tomo.rrow.11235@gmail.com>
2015-03-25 23:34:08 +0900

    add top

--- vendor/bundle/ruby/2.0.0/gems/eventmachine-1.0.7/ext/fastfilereader/siteconf20150310-944-17hp0xm.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20150310-944-13ebdnk"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
